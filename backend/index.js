53f40055a8994a6ca19ac903810db477a947a624
brazann <bzapata2002@gmail.com>
2024-09-06 00:49:46 -0400

    Backend Registro Usuario
    se creo un backend para el registro de los usuarios y tambien se ordenó dentro de carpetas

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { router: authRoutes, ensureAuthenticated } = require('./auth'); // Importa las rutas de autenticación
-const calendarRoutes = require('./calendar'); // Importa las rutas de Google Calendar
+const { router: authRoutes, ensureAuthenticated } = require('./routes/auth');
+const calendarRoutes = require('./routes/calendar');
+const userRoutes = require('./routes/users/user'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 // Rutas de autenticación (Google OAuth)
 app.use('/auth', authRoutes);
 
+// Rutas de usuario
+app.use('/users', userRoutes); // Usa las rutas de usuario en la ruta /users
+
 // Rutas protegidas para el calendario, que incluyen /create-event
 app.use('/api', ensureAuthenticated, calendarRoutes);
 
@@ -20,7 +24,6 @@ app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente!');
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
