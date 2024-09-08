3408533dd0c8c73223d601f211063f3b4e9e9f6c
Jose Jimenez Contreras <jjimenez2020@alu.uct.cl>
2024-09-07 22:31:18 -0400

    "Creando Login"

--- appAgenda/app/(tabs)/index.tsx
+++ appAgenda/app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { Image, StyleSheet, Button, View, Text, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, Button, View, Text, TextInput, } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 
 
@@ -10,7 +11,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-
+       <Text style={styles.titulo}>Inicio de Sesion</Text>
+       <Text style={styles.subTitle}>Accede a tu cuenta</Text>
+       <TextInput
+         placeholder="Rut"
+         style={styles.textInput}
+       />
+       <TextInput
+         placeholder="Contraseña"
+         style={styles.textInput}
+       />
+       <StatusBar style="auto"/>
     </View>
 
 
@@ -29,9 +40,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
     
   },
+   titulo:{
+      fontSize: 70,
+      color: '#34434D',
+      fontWeight: 'bold',
+
+
+   },
+   
+   subTitle: {
+     fontSize: 30,
+     color: 'gray',
+   },
+
+   textInput: {
+     
+     padding: 10,
+     paddingStart: 30,
+     width: '20%',
+     height: 50,
+     marginTop: 20,
+     borderRadius: 30,
+     backgroundColor: '#fff',
+
+
+
+
+
+   }
+
+
+
+
+
+
 });
