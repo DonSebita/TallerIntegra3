540f0537c5eb66eb5aeae632f4213f2416dec668
brazanzc <bzapata2021@alu.uct.cl>
2024-10-22 00:07:27 -0300

    Revert "dashboard profesionales y arreglos en backend"
    This reverts commit ea2ce254c6db29500d393ce7d4d92a6bb5143084.

--- Profesionales/app/_layout.tsx
+++ Profesionales/app/_layout.tsx
@@ -1,26 +1,12 @@
-import { AuthProvider, useAuth } from '@/components/services/authProvider';
-import { Slot, useRouter } from 'expo-router';
-import { useEffect } from 'react';
-
-function AuthRedirect() {
-  const { isAuthenticated } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.replace('/auth/login');
-    } else {
-      router.replace('/home');
-    }
-  }, [isAuthenticated, router]);
-
-  return <Slot />;
-}
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  
   return (
-    <AuthProvider>
-        <AuthRedirect />
-    </AuthProvider>
+    <Stack>
+      <Stack.Screen name="auth" options={{ headerShown: false }}/>
+      <Stack.Screen name="home"/>
+    </Stack>
   );
 }
+
