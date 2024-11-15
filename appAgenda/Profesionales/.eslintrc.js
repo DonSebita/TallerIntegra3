5cc97989e18c81fd23ab768ecd0e19623d6118f5
brazanzc <bzapata2021@alu.uct.cl>
2024-11-15 00:31:56 -0300

    Reinicio Front
    Se reinicia el front desde 0 para eliminacion de errores y facilitar implementacion de app Movil

--- appAgenda/Profesionales/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-// https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  extends: 'expo',
-  rules: {
-    'import/no-unresolved': 'off', // Desactiva la regla de módulos no resueltos
-  },
-};
--- appAgenda/backend/src/routes/citasRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { crearCita } = require('../controllers/citasController.js');
-const router = express.Router();
-
-// Ruta para la creación de una cita
-router.post('/crear-cita', crearCita);
-
-module.exports = router;
