8bf3687e754b8d7dff0bd9409184aa38761cfa85
DonSebita <luxioxbox49@gmail.com>
2024-11-25 23:54:36 -0300

    cambios para usar el backend en el servidor

--- Profesionales/app/_layout.tsx
+++ Profesionales/app/_layout.tsx
@@ -1,39 +1,39 @@
-// import { AuthProvider, useAuth } from '@/components/services/authProvider';
-// import { Slot, useRouter } from 'expo-router';
-// import { useEffect } from 'react';
+import { AuthProvider, useAuth } from '@/components/services/authProvider';
+import { Slot, useRouter } from 'expo-router';
+import { useEffect } from 'react';
 
-// function AuthRedirect() {
-//   const { isAuthenticated } = useAuth();
-//   const router = useRouter();
+function AuthRedirect() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
 
-//   useEffect(() => {
-//     if (!isAuthenticated) {
-//       router.replace('/auth');
-//     } else {
-//       router.replace('/home');
-//     }
-//   }, [isAuthenticated, router]);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace('/auth');
+    } else {
+      router.replace('/home');
+    }
+  }, [isAuthenticated, router]);
 
-//   return <Slot />;
-// }
-
-// export default function RootLayout() {
-//   return (
-//     <AuthProvider>
-//         <AuthRedirect />
-//     </AuthProvider>
-//   );
-// }
-
-
-import { Stack} from 'expo-router';
-
+  return <Slot />;
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name='auth/login' options={{headerShown:false,}}/>
-      <Stack.Screen name='home' options={{headerShown:false,}}/>
-    </Stack>
+    <AuthProvider>
+        <AuthRedirect />
+    </AuthProvider>
   );
 }
+
+
+// import { Stack} from 'expo-router';
+
+
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name='auth/login' options={{headerShown:false,}}/>
+//       <Stack.Screen name='home' options={{headerShown:false,}}/>
+//     </Stack>
+//   );
+// }
